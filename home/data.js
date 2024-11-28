6c80c53afb1004024da0e2b07f4ba24767951375
raychang1022 <raychang1022@gmail.com>
2024-11-28 11:49:01 +0800

    w

---
+++ home/data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "主門口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.4429041858602076,
+        "pitch": 0.08794364368309004,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.6670267870763364,
+          "pitch": -0.2578176977035369,
+          "title": "主門口(民國時期)",
+          "text": "在民國時期所新設立的大門"
+        }
+      ]
+    }
+  ],
+  "name": "首頁",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
---
+++ indoor/data.js
@@ -0,0 +1,263 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "室內一樓中央積木展",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 0.03033059121875681,
+        "pitch": 0.26878287900096254,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.377212489452635,
+          "pitch": 0.41102605188366503,
+          "rotation": 0.7853981633974483,
+          "target": "1-1"
+        },
+        {
+          "yaw": 1.1410385650267152,
+          "pitch": 0.5863684023109919,
+          "rotation": 6.283185307179586,
+          "target": "3-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.03033059121875681,
+          "pitch": 0.26878287900096254,
+          "title": "積木展",
+          "text": "利用積木建立了大量台灣特色建築"
+        },
+        {
+          "yaw": 2.162959774335935,
+          "pitch": 0.3156293436183777,
+          "title": "木雕",
+          "text": "由各個雕刻師所雕刻的木雕動物"
+        }
+      ]
+    },
+    {
+      "id": "1-1",
+      "name": "室內1樓雕像",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1047722984507633,
+          "pitch": -0.08545992365256438,
+          "rotation": 6.283185307179586,
+          "target": "4-2"
+        },
+        {
+          "yaw": 1.6657224376896425,
+          "pitch": 0.47890947046039756,
+          "rotation": 6.283185307179586,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-2",
+      "name": "室內2樓石雕",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1022396043234153,
+          "pitch": 0.4756012581613298,
+          "rotation": 0.7853981633974483,
+          "target": "4-2"
+        },
+        {
+          "yaw": -1.5167388764084908,
+          "pitch": 0.26904020222469605,
+          "rotation": 0,
+          "target": "3-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3884269889414327,
+          "pitch": 0.3433729466351956,
+          "title": "石製作品",
+          "text": "展覽著各石製物品"
+        },
+        {
+          "yaw": 1.5979605523391154,
+          "pitch": -0.02398974229152806,
+          "title": "餐廳",
+          "text": "走累後，能來此休息，並享用各色飲品，甜點"
+        }
+      ]
+    },
+    {
+      "id": "3-2",
+      "name": "室內2樓畫展",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7613383960440014,
+          "pitch": 0.21222729186463773,
+          "rotation": 0.7853981633974483,
+          "target": "2-2"
+        },
+        {
+          "yaw": 0.6533642476703712,
+          "pitch": 0.46873694799405463,
+          "rotation": 7.0685834705770345,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.10349419223056167,
+          "pitch": -0.07971380711558851,
+          "title": "畫",
+          "text": "用點畫的手法所製成的特色圖畫"
+        },
+        {
+          "yaw": -1.112377732354,
+          "pitch": 0.20286345209550483,
+          "title": "積木",
+          "text": "如同一樓的積木展，展現台灣特色景點"
+        }
+      ]
+    },
+    {
+      "id": "4-2",
+      "name": "室內2樓暴力熊",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -1.3485152146074597,
+        "pitch": 0.24325339161919146,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4368431038175178,
+          "pitch": 0.46196150536644787,
+          "rotation": 5.497787143782138,
+          "target": "2-2"
+        },
+        {
+          "yaw": 0.8180829366987385,
+          "pitch": 0.4922840128302308,
+          "rotation": 4.71238898038469,
+          "target": "1-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.11204911157469866,
+          "pitch": 0.15961446714237226,
+          "title": "暴力熊",
+          "text": "為展覽特色暴力熊的景點"
+        },
+        {
+          "yaw": 1.7163393967278076,
+          "pitch": -0.13349619963044645,
+          "title": "畫",
+          "text": "掛著各色的畫展"
+        }
+      ]
+    }
+  ],
+  "name": "司令院副建築室內展",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
