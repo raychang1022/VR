fa30d091cac162e9418c99d35e2b9df7b5521034
raychang1022 <raychang1022@gmail.com>
2024-11-28 11:28:17 +0800

    w

---
+++ outdoor/data.js
@@ -0,0 +1,293 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "戶外大石碑",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 0.06304607393176198,
+        "pitch": 0.034007554364833936,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6886586638560601,
+          "pitch": 0.19460174590173374,
+          "rotation": 6.283185307179586,
+          "target": "2-"
+        },
+        {
+          "yaw": 1.2054418705602217,
+          "pitch": 0.12230277142369772,
+          "rotation": 11.780972450961727,
+          "target": "1-"
+        },
+        {
+          "yaw": -0.008808697806479415,
+          "pitch": 0.3213919503028073,
+          "rotation": 6.283185307179586,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.018591428048365444,
+          "pitch": 0.03380437412213233,
+          "title": "石碑(蔣公題字)",
+          "text": "蔣公曾來此巡視，並在此立碑，上頭的砲彈，也代表著此為軍事重地"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "戶外石牌",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7145539402865158,
+          "pitch": 0.22898107517345423,
+          "rotation": 5.497787143782138,
+          "target": "2-"
+        },
+        {
+          "yaw": 2.4764127069211757,
+          "pitch": 0.254258124361769,
+          "rotation": 6.283185307179586,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.14556211527725615,
+          "pitch": 0.45937687448397035,
+          "title": "石牌",
+          "text": "講述清法戰爭的歷史及畫像，也象徵著此地歷史故事濃厚"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "戶外副門哨站",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -2.886132737652126,
+        "pitch": 0.1415192612480567,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1368497986814443,
+          "pitch": 0.09793810227000321,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -2.8351157630502932,
+          "pitch": 0.09630140351279692,
+          "rotation": 0.7853981633974483,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.32366112343656894,
+          "pitch": -0.037325836601809215,
+          "title": "哨站",
+          "text": "為原司令的門口，左右哨塔負責巡邏保衛之用。"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "要塞司令部古蹟主建築",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 0.020259674953191364,
+        "pitch": 0.028852001810301786,
+        "fov": 1.2241160510479343
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07564536338144201,
+          "pitch": 0.3172981648383164,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -3.075881994132347,
+          "pitch": 0.3120968517646787,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.11481336061607905,
+          "pitch": -0.5085294256885522,
+          "title": "司令部主建築",
+          "text": "為該司令部主要建築，主要講述基隆歷史、文化，並有部分親子休閒、娛樂產所，將不定期開放"
+        }
+      ]
+    },
+    {
+      "id": "4-1",
+      "name": "主建築1樓基隆史",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 0.1474042696350253,
+        "pitch": 0.2519935534513671,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.592396835998777,
+          "pitch": 0.40777496420566983,
+          "rotation": 0.7853981633974483,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6871335794075257,
+          "pitch": -0.1347863927216224,
+          "title": "歷史展覽",
+          "text": "講述基隆歷史及古早產業、文化"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "主建築走廊",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.651379995328293,
+        "pitch": 0.19695441455452922,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3517779156840657,
+          "pitch": 0.6655321079352383,
+          "rotation": 0.7853981633974483,
+          "target": "4-1"
+        },
+        {
+          "yaw": -1.6027367030969017,
+          "pitch": 0.6058885511427885,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.4820687903285332,
+          "pitch": -0.2911785209374216,
+          "title": "二樓",
+          "text": "有現代物品的展覽"
+        }
+      ]
+    }
+  ],
+  "name": "司令院戶外和主建築",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
